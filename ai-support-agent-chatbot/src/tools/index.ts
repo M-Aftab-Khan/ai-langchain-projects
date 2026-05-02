9207cb7914099cc6dda922368ef314183bcb7689
Aftab <aftabjsdev@gmail.com>
2026-05-02 16:25:07 +0500

    Added DB Query Tool

--- ai-support-agent-chatbot/src/tools/index.ts
+++ ai-support-agent-chatbot/src/tools/index.ts
@@ -1,4 +1,5 @@
 import { calculatorTool } from "./calculator.tool";
 import { weatherTool } from "./weather.tool";
+import { leadTool } from "./lead.tool";
 
-export const tools = [calculatorTool, weatherTool];
+export const tools = [calculatorTool, weatherTool, leadTool];
---
+++ ai-support-agent-chatbot/src/tools/lead.tool.ts
@@ -0,0 +1,41 @@
+import {tool} from "@langchain/core/tools";
+import {z} from "zod";
+import { prisma } from "../lib/prisma";
+
+export const leadTool = tool(
+    async({query}: {query: string}) => {
+        try{
+            const leads = await prisma.lead.findMany({
+                Where:{
+                    OR:[
+                        {name: {contains: query, mode: "insensitive"}},
+                        {email: {contains: query, mode: "insensitive"}},
+                    ]
+                },
+                take:5,
+            
+            });
+
+            if(leads.length === 0) {
+                return "No leads found";
+            }
+
+            return leads.map((lead:any) => ({
+                name: lead.name,
+                email: lead.email,
+                status: lead.status,
+               
+            }));
+        }catch(err:any) {
+            return "Error fetching leads";
+        }
+    },
+    {
+        name: "get_leads",
+        description:
+          "Search leads by name or email. Use when user asks about leads or customers.",
+        schema: z.object({
+          query: z.string(),
+        }),
+      }
+)
